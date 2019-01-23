18a257f2b1687f2be0996b4d18a5a78095e33ce9
Isaque Veras <46972789+isaqueveras@users.noreply.github.com>
2019-01-23 16:37:35 -0200

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
 	include_once("seguranca.php"); 
 	$conectar = mysql_connect("localhost","root","") or die ("Erro na conexão");
-	mysql_select_db("elchamado") or die ("Base não encontrado!");
+	mysql_select_db("baco_de_dados") or die ("Base não encontrado!");
 ?>
